5eeffe2d56a487c1bd07228c0dbbc9b57cce2e5b
Brian Jarvis <b@bjarvis.org>
2020-05-03 11:20:37 -0400

    Update piezo_midi_usb.ino
    Add some debug (because things aren't working)

--- piezo_midi_usb.ino
+++ piezo_midi_usb.ino
@@ -1,6 +1,9 @@
 #include "MIDIUSB.h"
 #include "PitchToNote.h"
 
+#define ENABLE_SERIAL_OUTPUT (1) //Uncomment this entire line to write debug info to serial
+//#define DISABLE_MIDI_OUTPUT (1) //Uncomment this entire line to disable MIDI output (useful in debugging)
+
 #define NUM_SENSORS (1)
 const byte sensor_pins[NUM_SENSORS] = {A1};
 const int sensor_thresholds[NUM_SENSORS] = {20};
@@ -28,8 +31,12 @@ const byte midi_channels[NUM_SENSORS] = {0}; //MIDI channel to output on per sen
 const byte sensor_notes[NUM_SENSORS] = {pitchC3};//, pitchD3, pitchE3, pitchF3, pitchG3, pitchA3, pitchB3, pitchC4, pitchD4, pitchE4, pitchF4, pitchG4}; //C major scale
 
 void setup() {
+#ifdef ENABLE_SERIAL_OUTPUT
+  Serial.begin(115200);
+#endif
   //Initialize the default values
   for (int sensor_num; sensor_num < NUM_SENSORS; sensor_num++) {
+    pinMode(sensor_pins[sensor_num], INPUT);
     pitches[sensor_num] = DEFAULT_PITCH;
     curr_val[sensor_num] = -1;
     last_val[sensor_num] = -1;
@@ -40,9 +47,7 @@ void setup() {
 }
 
 int read_value(int sensor_num) {
-  int val = 0;
-  val = analogRead(sensor_pins[sensor_num]);
-  return val;
+  return analogRead(sensor_pins[sensor_num]);
 }
 
 byte get_velocity_from_val(int val) {
@@ -52,26 +57,35 @@ byte get_velocity_from_val(int val) {
 
 void MIDINoteOn(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+#ifndef DISABLE_MIDI_OUTPUT
   MidiUSB.sendMIDI(noteOn);
+#endif
 }
 
 void MIDINoteOff(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+#ifndef DISABLE_MIDI_OUTPUT
   MidiUSB.sendMIDI(noteOff);
+#endif
 }
 
 void loop() {
   unsigned long curr_time = millis();
   for (int sensor_num = 0; sensor_num < NUM_SENSORS; sensor_num++) {
-
     //Read a new value and stash the previous one 
     last_val[sensor_num] = curr_val[sensor_num];
     curr_val[sensor_num] = read_value(sensor_num);
+
     //The curr and last values were initialized to -1, so don't do anything until these have been overwritten with real data
     if (last_val[sensor_num] == -1 || curr_val[sensor_num] == -1) {
       continue;
     }
-
+#ifdef ENABLE_SERIAL_OUTPUT
+    Serial.println("-------------------------------------------------------------------------------");
+    Serial.print("["); Serial.print(curr_time); Serial.print("] Sensor "); Serial.println(sensor_num);
+    Serial.print("---- Value: "); Serial.println(curr_val[sensor_num]);
+    Serial.print("---- Currently playing?: "); Serial.println(is_playing[sensor_num]);
+#endif
     if (is_playing[sensor_num]) {
       //If a note is playing currently, and there's still time left to play, do nothing for now
       if ((curr_time - note_on_time[sensor_num]) < note_duration_ms[NUM_SENSORS]) {
@@ -89,9 +103,15 @@ void loop() {
     //If the difference between two consecutive reads breaches the threshold, play a note.
     //There are different ways of determining whether to play something, so if this is behaving weirdly, look into using another approach.
     int value_diff = abs(last_val[sensor_num] - curr_val[sensor_num]);    
+#ifdef ENABLE_SERIAL_OUTPUT
+    Serial.print("---- Value diff from last read: "); Serial.println(value_diff);
+#endif
     if (value_diff > sensor_thresholds[sensor_num]) {
       pitches[sensor_num] = sensor_notes[sensor_num];
       velocities[sensor_num] = get_velocity_from_val(value_diff);
+#ifdef ENABLE_SERIAL_OUTPUT
+      Serial.print("---- Threshold breached. Playing pitch: "); Serial.print(pitches[sensor_num]); Serial.print(", velocity: "); Serial.println(velocities[sensor_num]);
+#endif
       MIDINoteOn(midi_channels[sensor_num],
                  pitches[sensor_num],
                  velocities[sensor_num]);
